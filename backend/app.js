681cbd8faa6c8c8c90451c74668c39fc0ddc8ec3
IjaazAhamed <ijaazijju1713@gmail.com>
2022-06-08 12:45:11 +0530

    final stage

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,41 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./connection/connect");
+const cors = require("cors");
+const dataRouter = require("./routes/data");
+dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/post",(req,res)=>{
-    res.send("welcome to real-estate properties")
-})
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.listen(5000,()=>{
-    console.log("listening server 5000...");
-})
+app.use(cors(corsOptions));
+
+const router = require("./routes/router");
+
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
+
+app.use("/", router);
+app.use(dataRouter);
+
+const startDB = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+
+    const port = process.env.PORT;
+    app.listen(port, () => {
+      console.log(`server is running and listening to port - ${port}`);
+      console.log("DB connected");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+startDB();
